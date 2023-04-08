2e860e2d40e0c516d5d6a8a5d508264830fcb6dd
suraj <suraj200981@yahoo.co.uk>
2023-04-08 14:28:55 +0100

    Initial commit

---
+++ src/main/java/com/example/microservice/currecyconversionservice/CurrecyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice.currecyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrecyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrecyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservice/currecyconversionservice/CurrecyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.currecyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrecyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
